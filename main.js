052f7b86aab6eb75c5395481c2a0ffcb226711ab
Bernaz Barros <bernazbarros2000@gmail.com>
2025-04-11 13:28:44 +0100

    Created a sidebar, changed the theme of my menu bar, and created a content area where users can work, now I will create a drag and drop or browse file function

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        frame: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         }
     });
     win.loadFile('index.html');
+
+    // Listeners para os botões da barra de título
+    ipcMain.on('minimize-window', () => {
+        win.minimize();
+    });
+
+    ipcMain.on('maximize-window', () => {
+        if (win.isMaximized()) {
+            win.unmaximize();
+        } else {
+            win.maximize();
+        }
+    });
+
+    ipcMain.on('close-window', () => {
+        win.close();
+    });
 }
 
 app.whenReady().then(createWindow);
